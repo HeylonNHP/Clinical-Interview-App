fe1c4a905e533b4783917cdf71cd1049a6276327
Heylon <heylon96@hotmail.com>
2021-02-28 17:53:12 +1000

    Create InterviewData class

---
+++ app/src/main/java/com/example/clinicalinterviewapp/InterviewData/InterviewData.java
@@ -0,0 +1,10 @@
+package com.example.clinicalinterviewapp.InterviewData;
+
+public class InterviewData {
+    private String interviewDate;
+    private String interviewTime;
+    private String suburb;
+    public InterviewData(){
+
+    }
+}

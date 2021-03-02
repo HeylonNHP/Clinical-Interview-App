51629f1202ec54ea2a951f64e516524a7059fa85
Heylon <heylon96@hotmail.com>
2021-03-02 21:56:10 +1000

    load interviewdata in InterviewSelection

--- app/src/main/java/com/example/clinicalinterviewapp/InterviewSelection.java
+++ app/src/main/java/com/example/clinicalinterviewapp/InterviewSelection.java
@@ -2,13 +2,25 @@ package com.example.clinicalinterviewapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.clinicalinterviewapp.InterviewData.InterviewData;
+import com.google.gson.Gson;
+
 public class InterviewSelection extends AppCompatActivity {
+    InterviewData interviewData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interview_selection);
+
+        Intent intent = getIntent();
+
+        Gson gson = new Gson();
+        this.interviewData = gson.fromJson(intent.getStringExtra("interviewData"),InterviewData.class);
+
+        System.out.println(this.interviewData.getInterviewDate());
     }
 }

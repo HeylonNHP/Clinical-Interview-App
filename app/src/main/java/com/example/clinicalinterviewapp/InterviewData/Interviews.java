f06c3e790f4d235ba94628feef2f36d472928875
Heylon <heylon96@hotmail.com>
2021-03-02 23:52:00 +1000

    Add test data to Interviews

--- app/src/main/java/com/example/clinicalinterviewapp/InterviewData/Interviews.java
+++ app/src/main/java/com/example/clinicalinterviewapp/InterviewData/Interviews.java
@@ -5,6 +5,32 @@ public class Interviews {
     public static Interview getMentalStateExaminationInterview(){
         Interview interview = new Interview();
         interview.setTitle("Mental State Examination Interview");
+
+        String category = "Appearance";
+
+        InterviewQuestion question = new InterviewQuestion();
+
+        question.setCategory(category);
+        question.setQuestion("Stature");
+
+        question.addAnswer("Average");
+        question.addAnswer("Small");
+        question.addAnswer("Tall");
+
+        interview.addQuestion(question);
+
+        question = new InterviewQuestion();
+
+        question.setCategory(category);
+        question.setQuestion("Weight for size");
+
+        question.addAnswer("Average weight");
+        question.addAnswer("Overweight");
+        question.addAnswer("Obese");
+        question.addAnswer("Underweight");
+
+        interview.addQuestion(question);
+
         return interview;
     }
 

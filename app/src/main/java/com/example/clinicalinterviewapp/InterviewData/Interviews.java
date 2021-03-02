819ddc14f74b16109181217a806b3e83210e4d20
Heylon <heylon96@hotmail.com>
2021-03-02 23:07:03 +1000

    Implement listview code for displaying items

---
+++ app/src/main/java/com/example/clinicalinterviewapp/InterviewData/Interviews.java
@@ -0,0 +1,11 @@
+package com.example.clinicalinterviewapp.InterviewData;
+
+public class Interviews {
+
+    public static Interview getMentalStateExaminationInterview(){
+        Interview interview = new Interview();
+        interview.setTitle("Mental State Examination Interview");
+        return interview;
+    }
+
+}
--- app/src/main/java/com/example/clinicalinterviewapp/InterviewSelection.java
+++ app/src/main/java/com/example/clinicalinterviewapp/InterviewSelection.java
@@ -4,9 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import com.example.clinicalinterviewapp.InterviewData.Interview;
 import com.example.clinicalinterviewapp.InterviewData.InterviewData;
+import com.example.clinicalinterviewapp.InterviewData.Interviews;
 import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class InterviewSelection extends AppCompatActivity {
     InterviewData interviewData;
@@ -22,5 +29,14 @@ public class InterviewSelection extends AppCompatActivity {
         this.interviewData = gson.fromJson(intent.getStringExtra("interviewData"),InterviewData.class);
 
         System.out.println(this.interviewData.getInterviewDate());
+
+        ListView listView = findViewById(R.id.interviewList);
+
+        List<Interview> interviewList = new ArrayList<>();
+        interviewList.add(Interviews.getMentalStateExaminationInterview());
+
+        ArrayAdapter<Interview> interviewArrayAdapter = new ArrayAdapter<>(this,R.layout.list_item,R.id.listTextView,interviewList);
+
+        listView.setAdapter(interviewArrayAdapter);
     }
 }

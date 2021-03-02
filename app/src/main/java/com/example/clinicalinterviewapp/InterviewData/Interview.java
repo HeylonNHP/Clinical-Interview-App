dd50ca02258fbc07d713e58c59aec420ba309058
Heylon <heylon96@hotmail.com>
2021-03-02 22:15:01 +1000

    Create Interview

---
+++ app/src/main/java/com/example/clinicalinterviewapp/InterviewData/Interview.java
@@ -0,0 +1,33 @@
+package com.example.clinicalinterviewapp.InterviewData;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Interview {
+    private List<InterviewQuestion> questionList = new ArrayList<>();
+    private String interviewTitle;
+    public Interview(){
+
+    }
+
+    public void addQuestion(InterviewQuestion question){
+        questionList.add(question);
+    }
+
+    public void removeQuestion(InterviewQuestion question){
+        questionList.remove(question);
+    }
+
+    public InterviewQuestion getQuestion(int index){
+        return questionList.get(index);
+    }
+
+    public void setTitle(String title){
+        this.interviewTitle = title;
+    }
+
+    public String getTitle(){
+        return this.interviewTitle;
+    }
+
+}

732963f5d8c089b4ae3100fb455121de6a93e76a
Heylon <heylon96@hotmail.com>
2021-03-07 16:30:15 +1000

    Initialise QuestionScreen

--- app/src/main/java/com/example/clinicalinterviewapp/QuestionScreen.java
+++ app/src/main/java/com/example/clinicalinterviewapp/QuestionScreen.java
@@ -2,13 +2,28 @@ package com.example.clinicalinterviewapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.clinicalinterviewapp.InterviewData.InterviewData;
+import com.example.clinicalinterviewapp.InterviewData.InterviewQuestion;
+import com.google.gson.Gson;
+
 public class QuestionScreen extends AppCompatActivity {
+    InterviewData interviewData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question_screen);
+
+        Intent intent = getIntent();
+
+        Gson gson = new Gson();
+        this.interviewData = gson.fromJson(intent.getStringExtra("interviewData"),InterviewData.class);
+
+        InterviewQuestion question = this.interviewData.nextQuestion();
+
+
     }
 }

e19a916952cfa3c158b7bf265b279d2dd2524548
Heylon <heylon96@hotmail.com>
2021-03-07 18:00:44 +1000

    QuestionScreen add onClick handler for answer selection

--- app/src/main/java/com/example/clinicalinterviewapp/QuestionScreen.java
+++ app/src/main/java/com/example/clinicalinterviewapp/QuestionScreen.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.SparseBooleanArray;
+import android.view.View;
 import android.widget.AbsListView;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -16,6 +19,7 @@ import com.google.gson.Gson;
 public class QuestionScreen extends AppCompatActivity {
     InterviewData interviewData;
     ListView answersList;
+    InterviewQuestion currentQuestion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +28,7 @@ public class QuestionScreen extends AppCompatActivity {
 
         this.answersList = findViewById(R.id.answersList);
         this.answersList.setChoiceMode(AbsListView.CHOICE_MODE_SINGLE);
+        this.answersList.setOnItemClickListener(this::onClickListItem);
 
         Intent intent = getIntent();
 
@@ -31,8 +36,21 @@ public class QuestionScreen extends AppCompatActivity {
         this.interviewData = gson.fromJson(intent.getStringExtra("interviewData"),InterviewData.class);
 
         InterviewQuestion question = this.interviewData.nextQuestion();
-
+        this.currentQuestion = question;
         displayQuestion(question);
+    }
+
+    private void onClickListItem(AdapterView<?> adapterView, View view, int i, long l) {
+        SparseBooleanArray checkedItems = answersList.getCheckedItemPositions();
+
+        System.out.println(checkedItems);
+
+        for(int iter = 0; iter < checkedItems.size(); ++iter){
+            int key = checkedItems.keyAt(iter);
+
+            System.out.println("Key:" + key + " Value:" + checkedItems.get(key) + " Ans:" + answersList.getItemAtPosition(key));
+            this.currentQuestion.setUserChoice(answersList.getItemAtPosition(key).toString());
+        }
     }
 
     private void displayQuestion(InterviewQuestion question){

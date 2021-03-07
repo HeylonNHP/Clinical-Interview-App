998dadda4d52946124a84798535f2365b95bdcc4
Heylon <heylon96@hotmail.com>
2021-03-07 17:34:44 +1000

    Add displayQuestion method to QuestionScreen

--- app/src/main/java/com/example/clinicalinterviewapp/QuestionScreen.java
+++ app/src/main/java/com/example/clinicalinterviewapp/QuestionScreen.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.AbsListView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.clinicalinterviewapp.InterviewData.InterviewData;
 import com.example.clinicalinterviewapp.InterviewData.InterviewQuestion;
@@ -11,11 +15,15 @@ import com.google.gson.Gson;
 
 public class QuestionScreen extends AppCompatActivity {
     InterviewData interviewData;
+    ListView answersList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question_screen);
+
+        this.answersList = findViewById(R.id.answersList);
+        this.answersList.setChoiceMode(AbsListView.CHOICE_MODE_SINGLE);
 
         Intent intent = getIntent();
 
@@ -24,6 +32,15 @@ public class QuestionScreen extends AppCompatActivity {
 
         InterviewQuestion question = this.interviewData.nextQuestion();
 
+        displayQuestion(question);
+    }
 
+    private void displayQuestion(InterviewQuestion question){
+        TextView questionText = findViewById(R.id.questionTextView);
+        questionText.setText(question.getQuestion());
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_single_choice,question.getAnswersList());
+
+        this.answersList.setAdapter(adapter);
     }
 }
